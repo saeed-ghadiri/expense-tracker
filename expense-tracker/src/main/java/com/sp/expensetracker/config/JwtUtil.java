b7314227105d9c21043d92428fbd51b0a0a64b03
saeed-ghadiri <saeidghadiri.gh@gmail.com>
2024-10-25 10:58:29 +0330

    add springdoc & fix jwt.secret issue

--- expense-tracker/src/main/java/com/sp/expensetracker/config/JwtUtil.java
+++ expense-tracker/src/main/java/com/sp/expensetracker/config/JwtUtil.java
@@ -3,6 +3,7 @@ package com.sp.expensetracker.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +13,15 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtil {
-
+    @Value("${jwt.secret}")
+    private String jwtSecret;
     private static final long ACCESS_TOKEN_VALIDITY = 1000 * 60 * 60 * 10; // 10 hours
     private static final long REFRESH_TOKEN_VALIDITY = 1000 * 60 * 60 * 24 * 10; // 10 days
-    private final String jwtSecret = "uX7@L3q2#nK9p4Z!$wR$8T5v1^Yj0bW&uX7@L3q";
-    private final Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+
 
     public String generateToken(UserDetails userDetails) {
+        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
@@ -28,6 +31,7 @@ public class JwtUtil {
     }
 
     public String generateRefreshToken(UserDetails userDetails) {
+        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
